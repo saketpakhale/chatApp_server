46a6a304f8ec7370dd931994ee88057e77a5a9cc
unknown <saket.pakhale1002@gmail.com>
2024-06-07 20:48:10 +0530

    added clientOrigin

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 let config = {
     MONGODB_URL: process.env.MONGODB_SECRET || "http://0.0.0.0.27017/chatappDB",
     JWT_SECRET: process.env.JWT_SECRET || 'jwtsecret',
-    clientOrigin: "http://localhost:3000",
+    clientOrigin: "https://chatapps-project.netlify.app/" || "http://localhost:3000",
     port: 3001
 }
 
